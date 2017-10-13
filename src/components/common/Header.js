f778042dd8659cf62f7dc5bfb8d9bb80ede2f554
Eudemonyu <yulihou@gmail.com>
2017-10-12 22:24:05 -0500

    Bug fixed
    update

---
+++ src/components/common/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+const styles = {
+  viewStyle: {
+    backgroundColor: 'rgb(76, 181, 246)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    // paddingTop: 15,
+    shadowColor: 'rgb(73, 68, 72)',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export { Header };
